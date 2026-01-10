27ac2682d571cb181f4b75127de6184c9b8b735e
Abhishekgds <sheetalmangla23@gmail.com>
2026-01-10 10:00:43 +0545

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,12 @@
+#Storing values
+tree1=20
+tree2=27
+tree3=19
+tree4=34
+tree5=40
+#finding the total of trees
+sum=tree1+tree2+tree3+tree4+tree5
+print("the sum of all 5 trees=",sum)
+#finding the average of trees
+aver=sum/5
+print("the average of all trees=",aver)
